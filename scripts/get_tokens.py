ae81ab868eb3507366b3ddd93e48facbdc16ce60
Carlos Arellano Casale <40230544+casareafer2908@users.noreply.github.com>
2022-05-08 16:57:58 +0800

    refactor
    refactor get tokens

--- scripts/get_tokens.py
+++ scripts/get_tokens.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-from brownie import SimpleCollectible, AdvancedCollectible, accounts, network, config
-from metadata import sample_metadata
-from scripts.helpful_scripts import get_breed
+from brownie import OneTMShowOff, accounts, network, config
 
 
 def main():
+    dev = accounts.add(config["wallets"]["from_key"])
     print("Working on " + network.show_active())
-    advanced_collectible = AdvancedCollectible[len(SimpleCollectible) - 1]
-    breakpoint()
-    number_of_advanced_collectibles = advanced_collectible.tokenCounter()
-    print(number_of_advanced_collectibles)
+    one_tm_show_off = OneTMShowOff[len(OneTMShowOff) - 1]
+    ammountOfNfts = one_tm_show_off.totalSupply()
+    print(f"Total NFTs Minted ==> {ammountOfNfts}")
