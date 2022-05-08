cf2aa8da724c123093cfd93867df8e73820a6134
Carlos Arellano Casale <40230544+casareafer2908@users.noreply.github.com>
2022-05-08 14:05:30 +0800

    mint script
    mint script

---
+++ nft/scripts/mint_oneTM_nft.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from brownie import OneTMShowOff, accounts, config
+from web3 import Web3
+
+# mint tokens
+mintAmmount = 2
+payable = mintAmmount * 0.01
+
+
+def main():
+    dev = accounts.add(config["wallets"]["from_key"])
+
+    # Get the latest deployed contract
+    one_tm_show_off = OneTMShowOff[len(OneTMShowOff) - 1]
+    print(f"{mintAmmount} tokens for {payable} ether")
+    one_tm_show_off.mint(
+        mintAmmount, {"from": dev, "value": Web3.toWei(payable, "ether")}
+    )
