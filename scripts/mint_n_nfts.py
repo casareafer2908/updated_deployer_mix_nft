c4b49d56c8ae16a2055bd931364d2b3cea37bbf0
Carlos Arellano Casale <40230544+casareafer2908@users.noreply.github.com>
2022-05-10 13:56:36 +0800

    Lol fix

--- scripts/mint_n_nfts.py
+++ scripts/mint_n_nfts.py
@@ -6,10 +6,6 @@ def deploy10():
     for n in range(toDeploy):
         print(f"run number {(n + 1)}")
         mint2()
-        if n < toDeploy:
-            print("done, execute next")
-        else:
-            print("job finished")
 
 
 def main():
