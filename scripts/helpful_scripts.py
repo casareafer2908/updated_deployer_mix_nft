7f4b528d7eab61fd43f5c6f44f48ddc256872fb0
Carlos Arellano Casale <40230544+casareafer2908@users.noreply.github.com>
2022-05-13 16:18:12 +0800

    remove unused
    remove unused

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -16,7 +16,6 @@ import time
 
 # Set a default gas price
 from brownie.network import priority_fee
-from pymerkle import MerkleTree
 
 OPENSEA_FORMAT = "https://testnets.opensea.io/assets/{}/{}"
 NON_FORKED_LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["hardhat", "development", "ganache"]
